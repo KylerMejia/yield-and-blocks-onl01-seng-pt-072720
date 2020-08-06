d3bded2cf981c450091c2e2413a97f6f3c57cd67
Kyler Mejia <kylermejia1@gmail.com>
2020-08-06 22:46:10 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -4,9 +4,9 @@ def hello_t(array)
       yield(array[i])
       i = i + 1
     end
-    array
+    
 end
-["Tim", "Tom", "Jim"]
+
 hello_t(["Tim", "Tom", "Jim"]) do |name|
   if name.start_with?("T")
     puts "Hi, #{name}"
